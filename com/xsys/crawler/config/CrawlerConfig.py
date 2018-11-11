979f2bb8f92bbb0bf2a6f11b17b6fb0a81eb833d
Yahav Hoffman <yahav.hoffman@americanwell.com>
2018-11-11 18:58:51 +0200

    CrawlerConfig.py - Increase Testing Coverage

--- com/xsys/crawler/config/CrawlerConfig.py
+++ com/xsys/crawler/config/CrawlerConfig.py
@@ -16,3 +16,7 @@ if __name__ == '__main__':
     conf = CrawlerConfig()
     assert conf.CHUNK_SIZE.__eq__(65*1024), 'Assert Failed -> CHUNK_SIZE is not {0}'.format(conf.CHUNK_SIZE)
     assert conf.SUFFIX.__eq__('.(encrypted)'), 'Assert failed -> SUFFIX is not {0}'.format(conf.SUFFIX)
+    assert conf.HEX_e.__eq__(hex(ord('e'))), 'Assert failed -> HEX_e is not {0}'.format(conf.HEX_e)
+    assert conf.HEX_d.__eq__(hex(ord('d'))), 'Assert failed -> HEX_d is not {0}'.format(conf.HEX_d)
+    assert conf.HEX_r.__eq__(hex(ord('r'))), 'Assert failed -> HEX_r is not {0}'.format(conf.HEX_r)
+    assert conf.HEX_l.__eq__(hex(ord('l'))), 'Assert failed -> HEX_l is not {0}'.format(conf.HEX_l)
